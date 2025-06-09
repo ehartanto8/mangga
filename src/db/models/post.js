6659bf4b15c0494c79af5b7a5f25a097e43b2cb9
ehartanto-vm-8 <70289383+ehartanto-vm-8@users.noreply.github.com>
2025-06-09 17:50:56 -0400

    init

---
+++ src/db/models/post.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from 'mongoose'
+
+const postSchema = new Schema(
+  {
+    title: String,
+    author: String,
+    contents: String,
+    tags: [String],
+  },
+  { timestamps: true },
+)
+
+export const Post = mongoose.model('post', postSchema)
