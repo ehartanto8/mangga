b81e80bd18d4fa792484aa055f67f99f770ca045
ehartanto-vm-8 <70289383+ehartanto-vm-8@users.noreply.github.com>
2025-08-23 21:42:04 -0400

    progress with create, update, and delete post

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { postsRoutes } from './routes/posts.js'
+import bodyParser from 'body-parser'
 
 const app = express()
-app.use(express.json())
+app.use(bodyParser.json())
 
 postsRoutes(app)
 
--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,4 +1,4 @@
-import { listAllPosts, listPostsByAuthor, listPostsByTag, getPostById } from "../services/posts.js";
+import { listAllPosts, listPostsByAuthor, listPostsByTag, getPostById, createPost, updatePost, deletePost } from "../services/posts.js";
 
 export function postsRoutes(app) {
     // Routes
@@ -34,6 +34,37 @@ export function postsRoutes(app) {
             return res.json(post)
         } catch (err) {
             console.error('error getting post', err)
+            return res.status(500).end()
+        }
+    })
+
+    app.post('/api/v1/posts', async (req, res) => {
+        try {
+            const post = await createPost(req.body)
+            return res.json(post)
+        } catch (err) {
+            console.error('error creating post', err)
+            return res.status(500).end()
+        }
+    })
+
+    app.patch('/api/v1/posts/:id', async (req, res) => {
+        try {
+            const post = await updatePost(req.params.id, req.body)
+            return res.json(post)
+        } catch (err) {
+            console.error('error updating post', err)
+            return res.status(500).end()
+        }
+    })
+
+    app.delete('/api/v1/posts/:id', async (req, res) => {
+        try {
+            const { deletedCount } = await deletePost(req.params.id)
+            if (deletedCount === 0) return res.sendStatus(404)
+            return res.status(204).end()
+        } catch (err) {
+            console.error('error deleting post', err)
             return res.status(500).end()
         }
     })
